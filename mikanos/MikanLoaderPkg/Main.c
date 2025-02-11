6f1cdf43a152008dbc7f4dd5f1d42acc8ada0f57
yk0112 <130746469+yk0112@users.noreply.github.com>
2025-02-11 17:34:44 +0000

    bootloader for the kernel

--- mikanos/MikanLoaderPkg/Main.c
+++ mikanos/MikanLoaderPkg/Main.c
@@ -6,6 +6,7 @@
 #include  <Protocol/SimpleFileSystem.h>
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
+#include  <Guid/FileInfo.h>
 
 struct MemoryMap {
     UINTN buffer_size;
@@ -104,6 +105,7 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
     Print(L"Hello, Mikan World!\n");
     
+    // Get memory map
     CHAR8 memmap_buf[4096*4];
     struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     GetMemoryMap(&memmap);
@@ -117,6 +119,45 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
 
+    // Boot loader for the kernel.
+    EFI_FILE_PROTOCOL* kernel_file;
+    root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_info_buffer[file_info_size];
+    kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+    EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+    UINTN kernel_file_size = file_info->FileSize;
+
+    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+    gBS->AllocatePages(
+        AllocateAddress, EfiLoaderData, 
+        (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr); // 1page = 4KiB(0x1000)
+    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
+    // Stop Boot service before the kernel starts. 
+    EFI_STATUS status;
+    status = gBS->ExitBootServices(image_handle, memmap.map_key);
+    if(EFI_ERROR(status)) {
+        status = GetMemoryMap(&memmap);
+        if(EFI_ERROR(status)) {
+            Print(L"failed to get memory map :%r\n", status);
+            while(1);
+        }
+        status = gBS->ExitBootServices(image_handle, memmap.map_key);
+        if(EFI_ERROR(status)) {
+            Print(L"Could not exit boot service: %r\n", status);
+            while(1);
+        }
+    }
+
+    // Starts the kernel
+    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    typedef void EntryPointType(void);
+    EntryPointType* entry_point = (EntryPointType*)entry_addr;
+    entry_point();
+    
     while(1);
     return EFI_SUCCESS;
 }
