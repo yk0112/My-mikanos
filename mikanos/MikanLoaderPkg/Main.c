8a249c939bb4801b5fdf2ab45ecb41e894f5792c
yk0112 <130746469+yk0112@users.noreply.github.com>
2025-03-03 05:03:16 +0000

    add Write Pixel function

--- mikanos/MikanLoaderPkg/Main.c
+++ mikanos/MikanLoaderPkg/Main.c
@@ -7,6 +7,7 @@
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
 #include  <Guid/FileInfo.h>
+#include "../kernel/frame_buffer_config.hpp"
 
 struct MemoryMap {
     UINTN buffer_size;
@@ -206,9 +207,15 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
     UINTN kernel_file_size = file_info->FileSize;
 
     EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    gBS->AllocatePages(
+    status = gBS->AllocatePages(
         AllocateAddress, EfiLoaderData, 
         (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr); // 1page = 4KiB(0x1000)
+    
+    if(EFI_ERROR(status)) {
+      Print(L"failed to allocate pages: %r", status);
+      Halt();
+    }
+
     kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
     Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
@@ -226,12 +233,33 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
             while(1);
         }
     }
+    
+    // Get frame buffer info
+    struct FrameBufferConfig config = {
+      (UINT8*)gop->Mode->FrameBufferBase,
+      gop->Mode->Info->PixelsPerScanLine,
+      gop->Mode->Info->HorizontalResolution,
+      gop->Mode->Info->VerticalResolution,
+      0
+    };
 
+    switch(gop->Mode->Info->PixelFormat) {
+      case PixelRedGreenBlueReserved8BitPerColor:
+        config.pixel_format = kPixelRGBResv8BitPerColor;
+        break;
+      case PixelBlueGreenRedReserved8BitPerColor:
+        config.pixel_format = kPixelBGRResv8BitPerColor;
+        break;
+      default:
+        Print(L"Unimplemented pixel format: %d\n", gop->Mode->Info->PixelFormat);
+        Halt();
+    }
+    
     // Starts the kernel
     UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
-    typedef void EntryPointType(UINT64, UINT64);
+    typedef void EntryPointType(const struct FrameBufferConfig*);
     EntryPointType* entry_point = (EntryPointType*)entry_addr;
-    entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
+    entry_point(&config);
     
     Print(L"All done\n");
     while(1);
