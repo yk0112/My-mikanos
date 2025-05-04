32935c280c894895e7d94e8bf83cb969f5623a24
yk0112 <130746469+yk0112@users.noreply.github.com>
2025-05-04 11:15:32 +0000

    support modifier key

--- kernel/keyboard.cpp
+++ kernel/keyboard.cpp
@@ -17,17 +17,48 @@ namespace {
         0,    0,    0,    0,    '/',  '*',  '-',  '+',
         '\n', '1',  '2',  '3',  '4',  '5',  '6',  '7',
         '8',  '9',  '0',  '.', '\\',  0,    0,    '=',
-      };
+    };
+
+    const char keycode_map_shifted[256] = {
+        0,    0,    0,    0,    'A',  'B',  'C',  'D', 
+        'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L', 
+        'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T', 
+        'U',  'V',  'W',  'X',  'Y',  'Z',  '!',  '@', 
+        '#',  '$',  '%',  '^',  '&',  '*',  '(',  ')',
+        '\n', '\b', 0x08, '\t', ' ',  '_',  '+',  '{', 
+        '}',  '|',  '~',  ':',  '"',  '~',  '<',  '>', 
+        '?',  0,    0,    0,    0,    0,    0,    0,  
+        0,    0,    0,    0,    0,    0,    0,    0,  
+        0,    0,    0,    0,    0,    0,    0,    0,  
+        0,    0,    0,    0,    '/',  '*',  '-',  '+',
+        '\n', '1',  '2',  '3',  '4',  '5',  '6',  '7', 
+        '8',  '9',  '0',  '.', '\\',  0,    0,    '=', 
+    };
+
+    // const int kLControlBitMask = 0b00000001u;
+    const int kLShiftBitMask   = 0b00000010u;
+    // const int kLAltBitMask     = 0b00000100u;
+    // const int kLGUIBitMask     = 0b00001000u;
+    // const int kRControlBitMask = 0b00010000u;
+    const int kRShiftBitMask   = 0b00100000u;
+    // const int kRAltBitMask     = 0b01000000u;
+    // const int kRGUIBitMask     = 0b10000000u;
 }
 
 
 void InitializeKeyboard(std::deque<Message>& msg_queue) {
     // msg_queueを経由せず、直接文字表示したほうが良いのでは？
     usb::HIDKeyboardDriver::default_observer = 
-        [&msg_queue](uint8_t keycode) {
+        [&msg_queue](uint8_t modifier, uint8_t keycode) {
+            const bool shift = (modifier & (kLShiftBitMask | kRShiftBitMask)) != 0;
+            char ascii = keycode_map[keycode];
+            if(shift) {
+                ascii = keycode_map_shifted[keycode];
+            }
             Message msg{Message::kKeyPush};
+            msg.arg.keyboard.modifier = modifier;
             msg.arg.keyboard.keycode = keycode;
-            msg.arg.keyboard.ascii = keycode_map[keycode];
+            msg.arg.keyboard.ascii = ascii;
             msg_queue.push_back(msg); 
         }; 
 }
